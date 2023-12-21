27a8e77b991060028898b417edef909b411c2fb3
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2023-12-21 14:44:19 +0530

    created login,register UI and functionality along with addition of context to the application

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
 
 const Layout = (props) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={props.description} />
+        <meta name="keywords" content={props.keywords} />
+        <meta name="author" content={props.author} />
+        <title>{props.title}</title>
+      </Helmet>
       <Header />
-      <main style={{ minHeight: "70vh" }}>{props.children}</main>
+      <main style={{ minHeight: "79.5vh" }}>
+        <Toaster />
+        {props.children}
+      </main>
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce",
+  description: "Ecommerce app to buy products online",
+  keywords: "phone, android, shirts, men, women",
+  author: "abhimaurya-dev",
 };
 
 export default Layout;
